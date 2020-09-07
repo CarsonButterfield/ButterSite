e33e1ad88e89175a34f591b1f76b0c4da7259c35
Carson Butterfield <carsonbutterfield98@gmail.com>
2020-09-07 13:29:40 -0700

    prepping for pull

--- buttersite/src/components/Graphs/SimplePie.jsx
+++ buttersite/src/components/Graphs/SimplePie.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { XYPlot, RadialChart } from 'react-vis'
-// this chart is meant for displaying % and other simple data sets, only accepts two angles 
+// this chart is meant for displaying percentages  
 const SimplePie = (props) => {
+    const percentage = ((props.setOne / (props.setOne + props.setTwo)) * 100).toFixed(2)
     const data = [{angle: props.setOne}, {angle: props.setTwo}]
 
     return(
-    
-            <RadialChart height={300} width={300} data={data}/>
+    <div className="simple-pie">
+            <p className="simple-pie-label">{percentage}% <br/> Active Users</p>
+            <RadialChart radius={125} innerRadius={100} height={250} width={250} data={data}/>
+    </div>
     
     )
 }
